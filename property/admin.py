abd1f62854acebe2b91ac8866f5aea6916058468
oZerro <leha.normatov@mail.ru>
2023-12-01 23:00:44 +0300

    fill with data field Flat.construction_year

--- property/admin.py
+++ property/admin.py
@@ -5,7 +5,7 @@ from .models import Flat
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['owner', 'town', 'address']
+    search_fields = ['owner', 'town', 'address', 'construction_year']
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
