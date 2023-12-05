cf85273b67857a05306d90521d6ce0f6c1df9afb
oZerro <leha.normatov@mail.ru>
2023-12-05 18:31:24 +0300

    setup admin UI for likes

--- property/models.py
+++ property/models.py
@@ -53,6 +53,9 @@ class Flat(models.Model):
 
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
+    like = models.ManyToManyField(
+        User,
+        verbose_name='Кто лайкнул')
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
